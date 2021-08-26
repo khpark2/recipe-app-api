dfd69df5ccd90ad8096fd7c1d1e4c09cf9651805
Keon Ho Park <khpark@Keons-MacBook-Air.local>
2021-08-26 22:04:55 +0900

    Added cusotm recipe detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.settings import reload_api_settings
 
 from core.models import Tag, Ingredient, Recipe
 
@@ -36,3 +37,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('title', 'title', 'ingredients', 'tags', 'time_minutes', 
                     'price', 'link')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
